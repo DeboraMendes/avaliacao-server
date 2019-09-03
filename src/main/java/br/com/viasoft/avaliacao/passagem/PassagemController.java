4453d92cddabf0cffc8dec4668d27e46d0b4e6ae
debora.mendes <debora.mendes@live.com>
2019-09-03 16:44:57 -0300

    OK

--- src/main/java/br/com/viasoft/avaliacao/passagem/PassagemController.java
+++ src/main/java/br/com/viasoft/avaliacao/passagem/PassagemController.java
@@ -1,12 +1,17 @@
 package br.com.viasoft.avaliacao.passagem;
 
+import br.com.viasoft.avaliacao.cidade.Cidade;
 import br.com.viasoft.avaliacao.crud.CrudController;
 import br.com.viasoft.avaliacao.crud.CrudService;
+import br.com.viasoft.avaliacao.diasDaSemana.DiaDaSemana;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/passagem")
@@ -19,6 +24,13 @@ public class PassagemController extends CrudController<Passagem, Long> {
     @Override
     protected CrudService<Passagem, Long> getService() {
         return passagemService;
+    }
+
+    @GetMapping("searchPassagem")
+    public List<Passagem> complete(@RequestParam("diaDaSemana") DiaDaSemana diaDaSemana,
+                                   @RequestParam("origem") Cidade origem,
+                                   @RequestParam("destino")Cidade destino) {
+        return passagemService.searchPassagem(diaDaSemana, origem, destino);
     }
 
 }
